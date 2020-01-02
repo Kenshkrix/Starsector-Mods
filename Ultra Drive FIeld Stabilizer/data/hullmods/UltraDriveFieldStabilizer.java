cea3bbb9e2de5e5e04da2820b6ca141f8900de87
Kenshkrix <42079267+Kenshkrix@users.noreply.github.com>
2020-01-02 02:32:42 -0800

    Added Ultra Drive Field Stabilizer mod
    Adds a cheat hullmod to maximize fleet speed.

---
+++ Ultra Drive FIeld Stabilizer/data/hullmods/UltraDriveFieldStabilizer.java
@@ -0,0 +1,20 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.impl.campaign.ids.Stats;
+
+public class UltraDriveFieldStabilizer extends BaseHullMod {
+
+	public static float BURN_BONUS = 40;
+	
+	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		stats.getDynamic().getMod(Stats.FLEET_BURN_BONUS).modifyFlat(id, BURN_BONUS);
+	}
+}
+
+
+
+
