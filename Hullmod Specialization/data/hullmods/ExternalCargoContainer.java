c473e7350e76f28d183d7ac05691324dceb2e23c
Kenshkrix <kenshkrix@gmail.com>
2021-06-06 07:30:40 -0400

    Update 1.16
    Added burn level modifier to External Cargo Container as originally intended.

--- Hullmod Specialization/data/hullmods/ExternalCargoContainer.java
+++ Hullmod Specialization/data/hullmods/ExternalCargoContainer.java
@@ -6,12 +6,15 @@ import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
 public class ExternalCargoContainer extends BaseHullMod {
 
+	private static final int BURN_LEVEL_MOD = -2;
 	public static final float SPEED_MOD = -15f;
 	public static final float MANEUVER_MOD = -15f;
 	public static final float ARMOR_MOD = -15f;
 	public static final float CARGO_MOD = 50f;
 	
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+		stats.getMaxBurnLevel().modifyFlat(id, BURN_LEVEL_MOD);
+		
 		stats.getMaxSpeed().modifyPercent(id, SPEED_MOD);
 		
 		stats.getAcceleration().modifyPercent(id, MANEUVER_MOD * 2f);
@@ -25,10 +28,11 @@ public class ExternalCargoContainer extends BaseHullMod {
 	}
 	
 	public String getDescriptionParam(int index, HullSize hullSize) {
-		if (index == 0) return "" + CARGO_MOD + "%";
-		if (index == 1) return "" + SPEED_MOD + "%";
-		if (index == 2) return "" + MANEUVER_MOD + "%";
-		if (index == 3) return "" + ARMOR_MOD + "%";
+		if (index == 0) return "" + BURN_LEVEL_MOD;
+		if (index == 1) return "" + CARGO_MOD + "%";
+		if (index == 2) return "" + SPEED_MOD + "%";
+		if (index == 3) return "" + MANEUVER_MOD + "%";
+		if (index == 4) return "" + ARMOR_MOD + "%";
 		return null;
 	}
 
