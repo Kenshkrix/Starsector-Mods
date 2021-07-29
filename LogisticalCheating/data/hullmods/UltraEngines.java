f415d5ace509e7ceabcbac5016bb75a12d1a72f4
Kenshkrix <kenshkrix@gmail.com>
2021-07-29 01:30:34 -0400

    Update 1.01
    Added combat speed bonus to Ultra Engines.

--- LogisticalCheating/data/hullmods/UltraEngines.java
+++ LogisticalCheating/data/hullmods/UltraEngines.java
@@ -9,6 +9,7 @@ public class UltraEngines extends BaseLogisticsHullMod {
 	
 	public static final int BURN_LEVEL_BONUS = 25;
 	public static final float MANEUVER_MOD = 45f;
+	public static final float SPEED_MOD = 30f;
 	
 	
 	public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
@@ -19,12 +20,15 @@ public class UltraEngines extends BaseLogisticsHullMod {
 		stats.getDeceleration().modifyMult(id, 1f + MANEUVER_MOD * 0.01f);
 		stats.getTurnAcceleration().modifyMult(id, 1f + MANEUVER_MOD * 0.01f);
 		stats.getMaxTurnRate().modifyMult(id, 1f + MANEUVER_MOD * 0.01f);
+		
+		stats.getMaxSpeed().modifyPercent(id, SPEED_MOD);
 	}
 	
 	public String getDescriptionParam(int index, HullSize hullSize) {
 		
 		if (index == 0) return "" + BURN_LEVEL_BONUS;
 		if (index == 1) return "" + ((int) MANEUVER_MOD) + "%";
+		if (index == 2) return "" + (int) SPEED_MOD + "%";
 		return null;
 	}
 
